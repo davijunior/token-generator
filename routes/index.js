234825460e3163e4b2ba557e4a3449f84821a78f
davi junior <martimiano.davi@gmail.com>
2020-03-13 13:29:58 -0300

    adicionando campo texto do card

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,24 @@ var multer = require('../tools/multer');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/nova-imagem', (req, res, next) => {
-  res.send(`
-      <html>
-          <head> 
-              <title> Nova imagem </title>
-          </head>
-          </body>
-              <!-- O enctype é de extrema importância! Não funciona sem! -->
-              <form action="/nova-imagem"  method="POST" enctype="multipart/form-data">
-                  <!-- O NAME do input deve ser exatamente igual ao especificado na rota -->
-                  <input type="file" name="image"><br>
-                  Nome: <input type="text" name="name"><br>
-                  Tipo: <input type="text" name="type" placeholder="Ex: Criatura - Elfo"> <br>
-                  Poder:<input type="text" name="power"><br>
-                  Resistência: <input type="text" name="thoughness"><br>
-                  Quantidade: <input type="number" name="times" min="1"><br>
-                  <button type="submit"> Enviar </button>
-              </form>
-          </body>
-      </html>
-  `);
-});
 router.post('/nova-imagem', multer.single('image'), (req, res, next) => {
-  var str = `<div class="card">
+  if(req.body.text.length > 0){
+        var str = `<div class="card">
+          <p class="title-card">${req.body.name}</p>
+          <img src='/images/${req.file.filename}'>
+          <p class="text-card">${req.body.text}</p>
+          <p class="type with-text">${req.body.type}</p>
+          <div class="label">${req.body.power}/${req.body.thoughness}</div>
+        </div>`;
+      }else{
+        var str = `<div class="card">
           <p class="title-card">${req.body.name}</p>
           <img src='/images/${req.file.filename}'>
           <p class="type">${req.body.type}</p>
           <div class="label">${req.body.power}/${req.body.thoughness}</div>
         </div>`;
+      }
+  
 
   if (req.file) {
       return res.send(`
